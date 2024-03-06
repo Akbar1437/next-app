f577a4ffdf4c268b218af35feac724dd6b656ec0
Akbar Khushnazarov <khusnazarovakbar@gmail.com>
2024-03-06 20:30:10 +0500

    add prisma adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import DiscordProvider from "next-auth/providers/discord";
 
 const prisma = new PrismaClient();
-
+const scopes = ["identify", "email", "applications.commands.permissions"];
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID!,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+      clientId: process.env.DISCORD_CLIENT_ID ?? "",
+      clientSecret: process.env.DISCORD_CLIENT_SECRET ?? "",
+      authorization: { params: { scope: scopes.join(" ") } },
     }),
   ],
 });
