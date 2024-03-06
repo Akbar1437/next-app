255195241e356765e74612f0e3e065ecd64d1ffe
Akbar Khushnazarov <khusnazarovakbar@gmail.com>
2024-03-06 19:33:57 +0500

    configure discord provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,15 @@
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+
+const prisma = new PrismaClient();
 
 const handler = NextAuth({
-  providers: [],
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    }),
+  ],
 });
-
 export { handler as GET, handler as POST };
