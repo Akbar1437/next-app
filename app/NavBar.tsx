3bc61b5c963ae9b98950162cbb6b69cac99c69e2
Akbar Khushnazarov <khusnazarovakbar@gmail.com>
2024-03-07 11:27:18 +0500

    refactor: navbar component

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,15 +1,41 @@
 "use client";
 import Link from "next/link";
+import { Skeleton } from "@/app/components";
 import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 const NavBar = () => {
+  return (
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify={"between"}>
+          <Flex align={"center"} gap={"3"}>
+            <Link href={"/"}>
+              <AiFillBug />{" "}
+            </Link>
+            <NavLinks />
+          </Flex>
+          <AuthStatus />
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+
+const NavLinks = () => {
   const currentPath = usePathname();
-  const { status, data: session } = useSession();
+
   const links = [
     {
       label: "Dashboard",
@@ -21,35 +47,58 @@ const NavBar = () => {
     },
   ];
   return (
-    <nav className="flex space-y-1 border-y mb-5  h-14">
-      <Link href={"/"} className="mt-2">
-        <AiFillBug />
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classNames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+  if (status === "loading") return <Skeleton width={"3rem"} />;
+  if (status === "unauthenticated") {
+    return (
+      <Link className="nav-link" href={"api/auth/signin"}>
+        Login
       </Link>
-      <ul className="flex space-x-6 px-5">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              className={classNames({
-                "text-zinc-900": link.href === currentPath,
-                "text-zinc-500": link.href !== currentPath,
-                "hover:text-zinc-800 transition-colors px-5": true,
-              })}
-              href={link.href}
-            >
-              {link.label}
-            </Link>
-          </li>
-        ))}
-        <Box>
-          {status === "authenticated" && (
+    );
+  }
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user?.image || "/public/vercel.svg"}
+            fallback="?"
+            size={"2"}
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size={"2"}>{session!.user?.name || "user"}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
             <Link href={"/api/auth/signout"}>Log out</Link>
-          )}
-          {status === "unauthenticated" && (
-            <Link href={"api/auth/signin"}>Login</Link>
-          )}
-        </Box>
-      </ul>
-    </nav>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
